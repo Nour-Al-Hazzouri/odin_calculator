679e542ee96de2838185bbce62341cd7a4ba57df
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-07 10:30:05 +0200

    Improve Calculator Design and input logic
    Improved the calculator design by changing the background color of the calculator's container.
    Added input logic which only accepts certain characters like numbers and operators to avoid errors.

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('input').addEventListener('keydown', e => {
+    const allowedChars = ['+', '-', '/', '*', '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace', 'ArrowLeft', 'ArrowRight']; // Define characters to prevent
+    if (!allowedChars.includes(e.key)) {
+        e.preventDefault(); // Prevent the character from being typed
+    }
+})
