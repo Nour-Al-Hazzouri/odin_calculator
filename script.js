46e2c143a5a21b2901921e31ebfd4938cd20dda6
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-07 12:58:45 +0200

    Improve Calculator Design and Script
    Improved the design and logic overall to make it stay in place in some zoom in and zoom out
    Added a comment in the script to explain the current piece of code and added some new allowed characters.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Input accepts only characters defined in the array
 document.querySelector('input').addEventListener('keydown', e => {
-    const allowedChars = ['+', '-', '/', '*', '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace', 'ArrowLeft', 'ArrowRight']; // Define characters to prevent
+    const allowedChars = ['+', '-', '/', '*', '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
+        'Backspace', 'ArrowLeft', 'ArrowRight']; // Define characters to prevent
     if (!allowedChars.includes(e.key)) {
         e.preventDefault(); // Prevent the character from being typed
     }
 })
+
