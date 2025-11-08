4e28fee46a2e7d1bd0aca21bc9c7c302aaffac1f
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-08 07:09:08 +0200

    Update Operators and Logic
    Updated the operators' buttons to actually insert operators.
    Updated the logic to handle floating point numbers in different operators
    Calculations are currently done through operators only, working to make '=' functional.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ input.addEventListener('keydown', e => {
 let number1= 0;
 let operator;
 let number2= 0;
+let result= 0;
 
 // Clear function by setting everything to NULL
 const clear= document.querySelector(".clear");
@@ -50,7 +51,7 @@ const operate= (number1, operator, number2) => {
     }
 };
 
-// Query all relevant buttons
+// Query all relevant numbers
 const numbers= document.querySelectorAll(".numbers-btn")
 
 // Logic to add numbers according to the requirements (floating number or not)
@@ -59,7 +60,7 @@ numbers.forEach(number => {
     let text;
     number.addEventListener("click", e => {
         content= e.target;
-        text= content.textContent;
+        text= content.textContent;  
         if (text === '=') {
             if (!number1 || !number2 || !operator)
                 alert("Make sure you selected 2 numbers and 1 operator")
@@ -72,3 +73,72 @@ numbers.forEach(number => {
     });
 });
 
+// Query all relevant operators
+const operators= document.querySelectorAll(".operators-btn");
+
+// Calculate all values if operators are directly clicked
+operators.forEach(operator => {
+    operator.addEventListener('click', e => {
+        content= e.target.textContent;
+        if (content === '+') {
+            if (!input.value.includes('+')) {
+                number1= input.value;
+                input.value += content;
+            } else if (input.value.includes('+')) {
+                let expression= input.value.split('+');
+                number2= expression[1];
+                if (input.value.includes('.')) {
+                    result= parseFloat(number1) + parseFloat(number2);
+                    input.value= result.toFixed(1);
+                } else {
+                    result= parseInt(number1) + parseInt(number2)
+                    input.value= result;
+                }
+            } 
+        }
+        if (content === '-') {
+            if (!input.value.includes('-')) {
+                number1= input.value;
+                input.value += content;
+            } else if (input.value.includes('-')) {
+                let expression= input.value.split('-');
+                number2= expression[1];
+                if (input.value.includes('.')) {
+                    result= parseFloat(number1) - parseFloat(number2);
+                    input.value= result.toFixed(1);
+                } else {
+                    result= parseInt(number1) - parseInt(number2)
+                    input.value= result;
+                }
+            }
+        }
+        if (content === '×') {
+            if (!input.value.includes('×')) {
+                number1= input.value;
+                input.value += content;
+            } else if (input.value.includes('×')) {
+                let expression= input.value.split('×');
+                number2= expression[1];
+                if (input.value.includes('.')) {
+                    result= parseFloat(number1) * parseFloat(number2);
+                    input.value= result.toFixed(1);
+                } else {
+                    result= parseInt(number1) * parseInt(number2)
+                    input.value= result;
+                }
+            }
+        }
+        if (content === '÷') {
+            if (!input.value.includes('÷')) {
+                number1= input.value;
+                input.value += content;
+            } else if (input.value.includes('÷')) {
+                let expression= input.value.split('÷');
+                number2= expression[1];
+                result= parseInt(number1) / parseInt(number2)
+                let rounded= result.toFixed(1)
+                input.value= parseFloat(rounded);
+            }
+        }
+    })
+})
