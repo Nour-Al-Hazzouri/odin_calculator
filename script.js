ba2a87395f48e145011fa86760410bbaff3105a9
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-08 12:48:59 +0200

    Complete Initial Logic
    Completed the initial logic for the calculator to function correctly
    Made code more modular and clean using functions.
    Further improvements and code will be for optimization purposes.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Query input element to modify and get numbers
 const input = document.querySelector("input");
+// Query all relevant operators
+const operators = document.querySelectorAll(".operators-btn");
+// Query all relevant numbers
+const numbers = document.querySelectorAll(".numbers-btn");
 // Input accepts only characters defined in the array
 input.addEventListener("keydown", (e) => {
   const allowedChars = [
@@ -27,7 +31,6 @@ input.addEventListener("keydown", (e) => {
 let number1 = 0;
 let operator;
 let number2 = 0;
-let result = 0;
 
 // Clear function by setting everything to NULL
 const clear = document.querySelector(".clear");
@@ -35,123 +38,102 @@ clear.addEventListener("click", () => {
   window.location.reload();
 });
 
-// The 4 main operations
-const add = (number1, number2) => {
-  return number1 + number2;
-};
-const subtract = (number1, number2) => {
-  return number1 - number2;
-};
-const multiply = (number1, number2) => {
-  return number1 * number2;
-};
-const divide = (number1, number2) => {
-  return number1 / number2;
-};
-
-// The main function to accept 2 numbers and 1 operator to do the math for the calculator.
-const operate = (number1, operator, number2) => {
-  if (operator === "+") {
-    return add(number1, number2);
-  }
-  if (operator === "-") {
-    return subtract(number1, number2);
-  }
-  if (operator === "*" || operator === "×") {
-    return multiply(number1, number2);
-  }
-  if (operator === "/" || operator === "÷") {
-    return divide(number1, number2);
-  }
-};
-
-// Query all relevant numbers
-const numbers = document.querySelectorAll(".numbers-btn");
-
-// Logic to add numbers according to the requirements (floating number or not)
+// Logic to add numbers according to the requirements (1 floating number or not)
 numbers.forEach((number) => {
   let content;
   let text;
   number.addEventListener("click", (e) => {
     content = e.target;
     text = content.textContent;
-    if (text === "=") {
-      if (!number1 || !number2 || !operator)
-        alert("Make sure you selected 2 numbers and 1 operator");
-    } else if (text === ".") {
+    if (text === ".") {
       input.value += text;
       number.disabled = true;
-    } else {
+    } else if (text !== "=") {
       input.value += text;
     }
   });
 });
 
-// Query all relevant operators
-const operators = document.querySelectorAll(".operators-btn");
+// Main 4 operations
+const add = (number1, number2) => {
+  if (input.value.includes(".")) {
+    result = parseFloat(number1) + parseFloat(number2);
+    input.value = result.toFixed(1);
+  } else {
+    result = parseInt(number1) + parseInt(number2);
+    input.value = result;
+  }
+};
+const subtract = (number1, number2) => {
+  if (input.value.includes(".")) {
+    result = parseFloat(number1) - parseFloat(number2);
+    input.value = result.toFixed(1);
+  } else {
+    result = parseInt(number1) - parseInt(number2);
+    input.value = result;
+  }
+};
+const multiply = (number1, number2) => {
+  if (input.value.includes(".")) {
+    result = parseFloat(number1) * parseFloat(number2);
+    input.value = result.toFixed(1);
+  } else {
+    result = parseInt(number1) * parseInt(number2);
+    input.value = result;
+  }
+};
+const divide = (number1, number2) => {
+  result = parseInt(number1) / parseInt(number2);
+  let rounded = result.toFixed(1);
+  input.value = parseFloat(rounded);
+};
+
+// Main operate function
+const operate = (number1, operator, number2) => {
+  if (operator === "+") return add(number1, number2);
+  else if (operator === "-") return subtract(number1, number2);
+  else if (operator === "×") return multiply(number1, number2);
+  else if (operator === "÷") return divide(number1, number2);
+};
+
+const separator = (operator) => {
+  if (!input.value.includes(operator)) {
+    number1 = input.value;
+    input.value += operator;
+  } else if (input.value.includes(operator)) {
+    let expression = input.value.split(operator);
+    number2 = expression[1];
+    operate(number1, operator, number2);
+  }
+};
 
 // Calculate all values if operators are directly clicked
 operators.forEach((operator) => {
   operator.addEventListener("click", (e) => {
     content = e.target.textContent;
+    // Start with actual calculation
     if (content === "+") {
-      if (!input.value.includes("+")) {
-        number1 = input.value;
-        input.value += content;
-      } else if (input.value.includes("+")) {
-        let expression = input.value.split("+");
-        number2 = expression[1];
-        if (input.value.includes(".")) {
-          result = parseFloat(number1) + parseFloat(number2);
-          input.value = result.toFixed(1);
-        } else {
-          result = parseInt(number1) + parseInt(number2);
-          input.value = result;
-        }
-      }
-    }
-    if (content === "-") {
-      if (!input.value.includes("-")) {
-        number1 = input.value;
-        input.value += content;
+      if (input)
+      separator(content);
+    } else if (content === "-") {
+      separator(content);
+    } else if (content === "×") {
+      separator(content);
+    } else if (content === "÷") {
+      separator(content);
+    } else {
+      if (input.value.includes("+")) {
+        index = input.value.indexOf("+");
+        separator(input.value[index]);
       } else if (input.value.includes("-")) {
-        let expression = input.value.split("-");
-        number2 = expression[1];
-        if (input.value.includes(".")) {
-          result = parseFloat(number1) - parseFloat(number2);
-          input.value = result.toFixed(1);
-        } else {
-          result = parseInt(number1) - parseInt(number2);
-          input.value = result;
-        }
-      }
-    }
-    if (content === "×") {
-      if (!input.value.includes("×")) {
-        number1 = input.value;
-        input.value += content;
-      } else if (input.value.includes("×")) {
-        let expression = input.value.split("×");
-        number2 = expression[1];
-        if (input.value.includes(".")) {
-          result = parseFloat(number1) * parseFloat(number2);
-          input.value = result.toFixed(1);
-        } else {
-          result = parseInt(number1) * parseInt(number2);
-          input.value = result;
-        }
-      }
-    }
-    if (content === "÷") {
-      if (!input.value.includes("÷")) {
-        number1 = input.value;
-        input.value += content;
+        index = input.value.indexOf("-");
+        separator(input.value[index]);
       } else if (input.value.includes("÷")) {
-        let expression = input.value.split("÷");
-        number2 = expression[1];
-        result = parseInt(number1) / parseInt(number2);
-        let rounded = result.toFixed(1);
-        input.value = parseFloat(rounded);
+        index = input.value.indexOf("÷");
+        separator(input.value[index]);
+      } else if (input.value.includes("×")) {
+        index = input.value.indexOf("×");
+        separator(input.value[index]);
       }
     }
   });
